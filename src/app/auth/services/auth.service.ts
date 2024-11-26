9588a4c625ea4426b6cde4e173676c2b159986a5
Irene San Roman <irenesrfuentes@gmail.com>
2024-11-26 01:08:34 +0100

    Fin Protección de Rutas

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../environments/environments';
 import { User } from '../interfaces/user.interface';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -13,7 +14,35 @@ export class AuthService {
 
   get currentUser():User|undefined {
     if (!this.user) return undefined;
-    return this.user;
+    return structuredClone(this.user);
   }
 
+  login(email: string, password: string): Observable<User> {
+
+    return this.http.get<User>(`${this.baseURL}/users/1`)
+    .pipe(
+      tap(user => this.user = user),
+      tap(user => localStorage.setItem('token', 'akskamkmz.ajsajs.askaksks'))
+    );
+  }
+
+  checkAuthentication(): Observable<boolean>{
+
+    if( !localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseURL}/users/1`)
+    .pipe(
+      tap(user => this.user = user),
+      map(user => !!user),
+      catchError(err => of(false))
+    )
+
+  }
+
+  logout(){
+    this.user = undefined;
+    localStorage.clear();
+  }
 }
